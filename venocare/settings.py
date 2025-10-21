7120c0c1a8e1b581495b78d27bac8e7ba4cb9c34
tanviedev <tanvintakle@gmail.com>
2025-10-21 23:04:28 +0530

    Remove unused CSS and HTML files

--- venocare/settings.py
+++ venocare/settings.py
@@ -156,7 +156,7 @@ import os
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR,
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
