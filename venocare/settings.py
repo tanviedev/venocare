501bd7087cedf84f9ba8cc5a4f93828aeb2c6cbc
Sagar Jamadar <sagarjamadar8989@gmail.com>
2026-03-21 13:54:43 +0530

    Fix typo in BASE_DIR assignment

--- venocare/settings.py
+++ venocare/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # ======================
